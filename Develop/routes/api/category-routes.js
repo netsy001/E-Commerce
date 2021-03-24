9e11c7c20ef6dc3138ba9a247ef5df4b82aae5a4
netsy001 <nettem.surendra@gmail.com>
2021-03-25 08:42:48 +1030

    changes to routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -33,10 +33,11 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
+    console.log(req.body)
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -61,7 +62,7 @@ router.put('/:id', (req, res) => {
   .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+
   // find all products
   // be sure to include its associated Category and Tag data
   router.get('/', async (req, res) => {
@@ -17,10 +17,9 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     }
   });
-});
+
 
 // get one product
-router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   router.get('/:id', async (req, res) => {
@@ -37,10 +36,10 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     }
   });
-});
+
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -56,6 +55,7 @@ router.post('/', (req, res) => {
       res.status(400).json(err);
     }
   });
+  router.post('/', async (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -76,7 +76,7 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+  });
 
 // update product
 router.put('/:id', (req, res) => {
@@ -120,7 +120,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
